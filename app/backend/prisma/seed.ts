3f944f10beb06281b3c1e123a938ca11058611e0
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-05-14 02:33:16 +0900

    [add] #3 お知らせの一覧表示、個別内容の閲覧機能を追加した

--- app/backend/prisma/seed.ts
+++ app/backend/prisma/seed.ts
@@ -1,81 +1,88 @@
-import { PrismaClient, User, Subsidy, Question } from '@prisma/client';
-import * as fs from 'fs';
-import * as path from 'path';
+import {
+  PrismaClient,
+  User,
+  Subsidy,
+  Question,
+  Information,
+  Answer,
+} from '@prisma/client'
+import * as fs from 'fs'
+import * as path from 'path'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 function getUsersFromJsonDirectory<T>(directoryPath: string): T[] {
-  const objects: T[] = [];
+  const objects: T[] = []
 
   fs.readdirSync(directoryPath).forEach((filename) => {
-    const filePath = path.join(directoryPath, filename);
-    const stat = fs.statSync(filePath);
+    const filePath = path.join(directoryPath, filename)
+    const stat = fs.statSync(filePath)
 
     if (stat.isFile() && path.extname(filename) === '.json') {
-      const data: T[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-      objects.push(...data);
+      const data: T[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+      objects.push(...data)
     }
-  });
+  })
 
-  return objects as T[];
+  return objects as T[]
 }
 
 const doSubsidySeed = async () => {
   const subsidies = await getUsersFromJsonDirectory<Subsidy>(
     'prisma/master/subsidy/chiba',
-  );
+  )
 
   for (const subsidy of subsidies) {
     await prisma.subsidy.create({
       data: {
         ...subsidy,
       },
-    });
+    })
   }
-};
+}
 
 const doUserSeed = async () => {
-  const users = await getUsersFromJsonDirectory<User>('prisma/master/users');
+  const users = await getUsersFromJsonDirectory<User>('prisma/master/users')
 
   for (const user of users) {
     await prisma.user.create({
       data: {
         ...user,
       },
-    });
+    })
   }
-};
+}
 
 const doPrefectureSeed = async () => {
   interface PrefectureInJsonData {
-    prefCode: number;
-    prefName: string;
+    prefCode: number
+    prefName: string
   }
   const prefectures = await getUsersFromJsonDirectory<PrefectureInJsonData>(
     'prisma/master/prefectures',
-  );
+  )
 
   for (const prefecture of prefectures) {
     await prisma.prefecture.create({
       data: {
         name: prefecture.prefName,
       },
-    });
+    })
   }
-};
+}
 
 const doManicipalitySeed = async () => {
   interface MunicipalityInJsonData {
-    prefCode: number;
-    cityCode: string;
-    cityName: string;
-    bigCityFlag: string;
+    prefCode: number
+    cityCode: string
+    cityName: string
+    bigCityFlag: string
   }
 
   const municipalities =
     await getUsersFromJsonDirectory<MunicipalityInJsonData>(
       'prisma/master/municipalities',
-    );
+    )
 
   for (const municipality of municipalities) {
     await prisma.municipality.create({
@@ -84,43 +91,71 @@ const doManicipalitySeed = async () => {
         municipalSymbolPath: `/src/public/munisipality-symbol/${municipality.cityCode}`,
         prefectureId: municipality.prefCode,
       },
-    });
+    })
   }
-};
+}
 
 const doQuestionsSeed = async () => {
   const questions = await getUsersFromJsonDirectory<Omit<Question, 'id'>>(
     'prisma/master/questions',
-  );
+  )
 
   for (const question of questions) {
     await prisma.question.create({
       data: {
         ...question,
       },
-    });
+    })
   }
-};
+}
+
+const doInforamtionsSeed = async () => {
+  const informations = await getUsersFromJsonDirectory<Omit<Information, 'id'>>(
+    'prisma/master/informations',
+  )
+
+  for (const information of informations) {
+    await prisma.information.create({
+      data: {
+        ...information,
+      },
+    })
+  }
+}
+
+const doAnswersSeed = async () => {
+  const answers = await getUsersFromJsonDirectory<Omit<Answer, 'id'>>(
+    'prisma/master/answers',
+  )
+
+  for (const answer of answers) {
+    await prisma.answer.create({
+      data: {
+        ...answer,
+      },
+    })
+  }
+}
 
 const main = async () => {
-  console.log(`Start seeding ...`);
+  console.log(`Start seeding ...`)
 
-  await doUserSeed();
-  await doPrefectureSeed();
-  await doManicipalitySeed();
-  await doSubsidySeed();
-  await doQuestionsSeed();
-  // await doHousingSubsidyCondtion();
-  // await doChildBirthSubsidyCondtion();
+  await doUserSeed()
+  await doPrefectureSeed()
+  await doManicipalitySeed()
+  await doSubsidySeed()
+  await doQuestionsSeed()
+  await doInforamtionsSeed()
+  await doAnswersSeed()
 
-  console.log(`Seeding finished.`);
-};
+  console.log(`Seeding finished.`)
+}
 
 main()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
--- app/backend/src/information/information.service.ts
+++ app/backend/src/information/information.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class InformationService {}
+export class InformationService {
+  constructor(private readonly prismaService: PrismaService) {}
+  /** お知らせをすべて取得する */
+  async getAllInformaitons() {
+    const informaitons = await this.prismaService.information.findMany({
+      orderBy: { id: 'desc' },
+    })
+
+    return informaitons
+  }
+
+  /** お知らせを一つ取得する */
+  async getOneInformation(informaitonId: number) {
+    const information = await this.prismaService.information.findFirst({
+      where: {
+        id: informaitonId,
+      },
+    })
+
+    return information
+  }
+}
