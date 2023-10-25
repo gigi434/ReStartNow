a8df6481ed6f9f73cd07772fe0c75e8daefe2ca9
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-10-25 16:07:38 +0900

    [update] #30 地域検索ページにて表示される市町区村画像のクレジット表記を追加した

--- app/frontend/src/components/organisms/RegionSearchForm/index.stories.tsx
+++ app/frontend/src/components/organisms/RegionSearchForm/index.stories.tsx
@@ -1,4 +1,5 @@
 import { mockMunicipalities } from '@/mocks/api/municipalities/mockData'
+import { mockPrefetures } from '@/mocks/api/prefectures/mockData'
 import { RegionSearchForm } from '@/src/components'
 import type { Meta, StoryObj } from '@storybook/react'
 const meta: Meta<typeof RegionSearchForm> = {
@@ -14,5 +15,6 @@ type Story = StoryObj<typeof RegionSearchForm>
 export const Default: Story = {
   args: {
     municipalities: mockMunicipalities,
+    prefectures: mockPrefetures
   },
 }
