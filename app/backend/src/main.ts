b3ba08c553a8e119d0d6ce19d75d797602e73803
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-10-31 17:41:45 +0900

    [remove] #52 mainファイルのSessionCookieにかかわる処理をコメントアウトすることで利用を保留とした

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,40 +1,40 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import RedisStore from 'connect-redis'
-import session from 'express-session'
-import Redis from 'ioredis'
-import passport from 'passport'
+// import RedisStore from 'connect-redis'
+// import session from 'express-session'
+// import Redis from 'ioredis'
+// import passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // whitelistはリクエストからdtoに定義されていないメンバーを削除する
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
-  // Redisの設定
-  const redisClient = new Redis(process.env.REDIS_URL)
-  await new Promise((resolve) => redisClient.on('connect', resolve))
-  const redisStore = new RedisStore({
-    client: redisClient,
-  })
-  app.use(
-    session({
-      name: 'session-id',
-      store: redisStore, // RedisStoreを初期化する
-      secret: process.env.REDIS_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'none',
-        path: '/',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7days,
-      },
-    }),
-  )
-  app.use(passport.initialize())
-  app.use(passport.session())
+  // // Redisの設定
+  // const redisClient = new Redis(process.env.REDIS_URL)
+  // await new Promise((resolve) => redisClient.on('connect', resolve))
+  // const redisStore = new RedisStore({
+  //   client: redisClient,
+  // })
+  // app.use(
+  //   session({
+  //     name: 'session-id',
+  //     store: redisStore, // RedisStoreを初期化する
+  //     secret: process.env.REDIS_SECRET,
+  //     resave: false,
+  //     saveUninitialized: false,
+  //     cookie: {
+  //       httpOnly: true,
+  //       secure: false,
+  //       sameSite: 'none',
+  //       path: '/',
+  //       maxAge: 7 * 24 * 60 * 60 * 1000, // 7days,
+  //     },
+  //   }),
+  // )
+  // app.use(passport.initialize())
+  // app.use(passport.session())
 
   // CORSの設定
   app.enableCors({
