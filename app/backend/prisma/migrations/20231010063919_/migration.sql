7b9ab801302dba95ed5720aaa20db92fc73b6d19
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-11-06 17:14:38 +0900

    Revert "[fix] #53 dotenvが開発環境以外のスクリプトに使用されていたため修正した"
    This reverts commit 0a0a2fbf8bd7c93a8cc34e07eb694bb574714ed0.

--- app/backend/prisma/migrations/20231010063919_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ageLimit` on the `Subsidy` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Subsidy" DROP COLUMN "ageLimit";
--- app/backend/prisma/migrations/20231029074133_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `validation` on the `Question` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Question" DROP COLUMN "validation";
