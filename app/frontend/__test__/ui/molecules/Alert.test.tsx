9ea6776aa852a4b34d44c26de94afbbcabfc781b
wagatsuma.yuichi <firefly.of.april@gmail.com>
2024-04-03 23:36:55 +0900

    [add] #145 #147での変更を取り込んだ

---
+++ app/frontend/__test__/ui/molecules/Alert.test.tsx
@@ -0,0 +1,17 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import { Alert } from '@/src/components/molecules'
+import React from 'react'
+
+describe('molecules/Alert', () => {
+  const content = 'test content'
+
+  beforeEach(() => {
+    render(<Alert />)
+  })
+
+  it('should be visible', () => {
+    expect(screen.getByText(content))
+  })
+
+  it('should be not visible when clicked onClose callback function', () => {})
+})
