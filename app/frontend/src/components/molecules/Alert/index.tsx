177f29091cfbee79dfbaa458b7f7f0f2e1dd5063
wagatsuma.yuichi <firefly.of.april@gmail.com>
2024-04-03 23:58:39 +0900

    [add] #145 Alertでの単体テストを追加した

--- app/frontend/src/components/molecules/Alert/index.tsx
+++ app/frontend/src/components/molecules/Alert/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import MuiAlert, { AlertProps as MuiAlertProps } from '@mui/material/Alert'
 
-export function Alert({ onClose, severity, variant, children }: MuiAlertProps) {
+export function Alert({
+  onClose,
+  severity = 'info',
+  variant = 'standard',
+  children,
+}: MuiAlertProps) {
   return (
     <MuiAlert variant={variant} onClose={onClose} severity={severity}>
       {children}
