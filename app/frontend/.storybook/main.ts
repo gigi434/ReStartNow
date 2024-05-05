341144ab1ab18504dcbc30415686158c6fcd1add
wagatsuma.yuichi <firefly.of.april@gmail.com>
2024-05-06 04:51:52 +0900

    [fix] #145 テスト用のreduxヘルパー関数の使用によりStorybookビルドエラーが発生するため修正した

--- app/frontend/.storybook/main.ts
+++ app/frontend/.storybook/main.ts
@@ -19,5 +19,19 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: ['../public'],
+  /* テスト用のヘルパー関数）を導入するとStorybookのビルドが失敗する。
+  https://redux.js.org/usage/writing-tests#setting-up-a-reusable-test-render-function
+  そのため、ワークアラウンドとして下記設定を導入する
+  https://github.com/vercel/next.js/issues/55620#issuecomment-1758276652
+  Storybookのビルド時にはワーニングが出るが、支障はない
+  WARN export 'render' (imported as 'render') was not found in '@testing-library/react' (possible exports: buildQueries, configure, ...
+  */
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = config.resolve.alias || {}
+    config.resolve.alias['@testing-library/react'] =
+      '@storybook/testing-library'
+    return config
+  },
 }
 export default config
